7ea34c726df8b8dae115d53b2f588f806076b4ff
JosephRios7 <joseph.rios@unl.edu.ec>
2024-05-14 00:24:29 -0500

    Dorsales en video

--- src/app.py
+++ src/app.py
@@ -7,6 +7,8 @@ from utils import get_true_annot, get_rbns, annotate
 from functions import run_all_tests, validate_accuracy
 
 # Función para cargar los estilos CSS
+
+
 def load_css(file_name):
     with open(file_name, "r") as f:
         css = f.read()
@@ -28,9 +30,11 @@ nr_classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 true_color = [66, 245, 108]
 color = [66, 245, 108]
 pred_color = [255, 0, 255]
-text_color = (66, 245, 108) 
+text_color = (66, 245, 108)
 dorsal_number = 0
 st.title('Detección de dorsales de corredores')
+
+
 def detect_dorsal():
     # Cargar la imagen directamente en el área principal
     uploaded_file = st.file_uploader("Cargar imagen", type=['jpg', 'jpeg'])
@@ -45,17 +49,17 @@ def detect_dorsal():
 
         # Mostrar la imagen cargada
         st.image(img, caption='Imagen cargada',
-                use_column_width=True, channels="BGR")
+                 use_column_width=True, channels="BGR")
 
         # Botón para ejecutar la detección
         if st.button('Ejecutar detección'):
             start = time.time()
             output = get_rbns(img, bd_configPath, bd_weightsPath,
-                            bd_classes, nr_configPath, nr_weightsPath, nr_classes)
+                              bd_classes, nr_configPath, nr_weightsPath, nr_classes)
             end = time.time()
 
             st.write(f'Pred time: {round(end - start, 2)} seconds')
-            
+
             if output:
                 dorsal_number = output[0][0]  # Extraer el número del dorsal
                 st.success(f'Número de dorsal: {dorsal_number}')
@@ -73,18 +77,18 @@ def detect_dorsal():
                 resized_img = cv.resize(
                     img, (3 * img_w, 3 * img_h), interpolation=cv.INTER_CUBIC)
                 RGB_im = cv.cvtColor(resized_img, cv.COLOR_BGR2RGB)
-                
+
                 # Mostrar la imagen con anotaciones
                 st.image(RGB_im, caption='Imagen con detecciones',
-                        use_column_width=True)
-                
+                         use_column_width=True)
+
                 # Obtener la fecha y hora actuales
                 now = datetime.datetime.now()
                 formatted_time = now.strftime("%d-%m-%y_%H_%M_%S")
 
                 # Formatear el nombre del archivo
                 filename = f"{dorsal_number}_{formatted_time}.jpg"
-        
+
             # Guardar la imagen procesada en la carpeta especificada
                 save_path = '../usr/validation/Full'
                 if not os.path.exists(save_path):
@@ -92,9 +96,9 @@ def detect_dorsal():
 
                 output_filename = os.path.join(save_path, filename)
                 cv.imwrite(output_filename, img)
-                
+
                 st.success(f'Imagen guardada en: {output_filename}')
-                
+
             else:
                 st.warning('No se detectaron dorsales en la imagen cargada.')
 
@@ -105,17 +109,16 @@ def detect_dorsal():
 # Botón para ejecutar la detección
 def model_update():
     if st.button('Ejecutar modelo'):
-            run_all_tests()
-            accuracy = validate_accuracy()
-            st.success(f'Detección completada. Precisión: {accuracy:.2f}')
+        run_all_tests()
+        accuracy = validate_accuracy()
+        st.success(f'Detección completada. Precisión: {accuracy:.2f}')
 
-            # # Mostrar la imagen anotada después de la detección
-            # annotated_image = cv.imread('/usr/validation/Full/imagen_annot.JPG')
-            # st.image(annotated_image, caption='Imagen anotada',
-            #          use_column_width=True)
-    
+        # # Mostrar la imagen anotada después de la detección
+        # annotated_image = cv.imread('/usr/validation/Full/imagen_annot.JPG')
+        # st.image(annotated_image, caption='Imagen anotada',
+        #          use_column_width=True)
+
 # Sección para ver las imágenes guardadas
-
 
 
 def view_saved_images():
@@ -146,10 +149,100 @@ def view_saved_images():
                          caption=img, use_column_width=True)
 
 
+# Función para cargar un video y procesarlo
+def detect_dorsal_from_video(video_path):
+
+    cap = cv.VideoCapture(video_path)
+    frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
+    if frame_count is not None:
+
+        # Iterar sobre los fotogramas del video
+        for _ in range(frame_count):
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Realizar la detección en cada fotograma
+            output = get_rbns(frame, bd_configPath, bd_weightsPath,
+                              bd_classes, nr_configPath, nr_weightsPath, nr_classes)
+
+            # if output:
+            #     dorsal_number = output[0][0]  # Extraer el número del dorsal
+            #     st.success(f'Número de dorsal: {dorsal_number}')
+            # else:
+            #     st.warning('No se detectaron dorsales en este fotograma.')
+
+            # Dibujar las detecciones en el fotograma si hay alguna
+            if output is not None:
+                for detection in output:
+                    frame = annotate(frame, detection, color)
+
+                frame_h, frame_w = frame.shape[:2]
+                resized_frame = cv.resize(
+                    frame, (3 * frame_w, 3 * frame_h), interpolation=cv.INTER_CUBIC)
+                RGB_frame = cv.cvtColor(resized_frame, cv.COLOR_BGR2RGB)
+
+                # Mostrar el fotograma con las detecciones
+                st.image(RGB_frame,
+                         use_column_width=True)
+
+            # else:
+            #     st.warning('No se detectaron dorsales en este fotograma.')
+
+        cap.release()
+    else:
+        st.warning('Por favor, carga un video para comenzar la detección.')
+# Función para cargar un video y procesarlo
+# def detect_dorsal_from_video(video_path):
+#     cap = cv.VideoCapture(video_path)
+#     frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+
+#     output_frames = []  # Lista para almacenar los fotogramas procesados
+
+#     # Iterar sobre los fotogramas del video
+#     for _ in range(frame_count):
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         # Realizar la detección en cada fotograma
+#         output = get_rbns(frame, bd_configPath, bd_weightsPath,
+#                           bd_classes, nr_configPath, nr_weightsPath, nr_classes)
+
+#         # Dibujar las detecciones en el fotograma si hay alguna
+#         if output is not None:
+#             for detection in output:
+#                 frame = annotate(frame, detection, color)
+
+#         # Convertir el fotograma a RGB y agregarlo a la lista de fotogramas procesados
+#         RGB_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+#         output_frames.append(RGB_frame)
+
+#     cap.release()
+
+#     # Guardar los fotogramas procesados como un video
+#     if output_frames:
+#         output_video_path = "output_video.mp4"
+#         out = cv.VideoWriter(output_video_path, cv.VideoWriter_fourcc(
+#             *"mp4v"), 30, (output_frames[0].shape[1], output_frames[0].shape[0]))
+#         for frame in output_frames:
+#             out.write(frame)
+#         out.release()
+
+#         # Mostrar el video final en Streamlit
+#         video_file = open(output_video_path, 'rb')
+#         video_bytes = video_file.read()
+#         st.video(video_bytes)
+#     else:
+#         st.warning('No se detectaron dorsales en el video cargado.')
 # Crear una barra de navegación en el sidebar
+
+
 def create_navbar():
     st.sidebar.title("Menú")
-    page = st.sidebar.radio("Ir a", ("Inicio", "Galería de Imágenes", "Actualizar Modelo", "Cámara Web"))
+    page = st.sidebar.radio("Ir a", ("Inicio", "Galería de Imágenes",
+                            "Actualizar Modelo", "Cámara Web", "Cargar Video"))
 
     if page == "Inicio":
         detect_dorsal()
@@ -159,26 +252,31 @@ def create_navbar():
         model_update()
     elif page == "Cámara Web":
         capture_camera()
-
-
-
-# Mostrar la barra de navegación y el contenido de la página
-create_navbar()
+    elif page == "Cargar Video":
+        uploaded_video = st.file_uploader("Cargar video", type=['mp4'])
+        if uploaded_video is not None:
+            video_path = "temp_video.mp4"
+            with open(video_path, "wb") as f:
+                f.write(uploaded_video.read())
+            detect_dorsal_from_video(video_path)
 
 
 # FUNCION DE DETECCION POR CAMARA WEB
-#----------------------------------------------------------------
+# ----------------------------------------------------------------
 # Selección de la cámara
 # Selección de la cámara
-camera_option = st.radio("Selecciona la cámara:", ('Cámara web integrada', 'Cámara externa'))
 
-if camera_option == 'Cámara web integrada':
-    cam_id = 0
-else:
-    cam_id = 1
 
 # Función para capturar video de la cámara web
 def capture_camera():
+    camera_option = st.radio("Selecciona la cámara:",
+                             ('Cámara web integrada', 'Cámara externa'))
+
+    if camera_option == 'Cámara web integrada':
+        cam_id = 0
+    else:
+        cam_id = 1
+
     if st.button('Iniciar Camara'):
         cap = cv.VideoCapture(cam_id)
         frame_st = st.empty()
@@ -194,9 +292,10 @@ def capture_camera():
 
             # Mostrar el frame con predicciones
             frame_st.image(frame_with_predictions, caption='Detección en tiempo real',
-                        use_column_width=True, channels="BGR")
+                           use_column_width=True, channels="BGR")
 
-            time.sleep(0.1)  # Añadir un pequeño retraso para mejorar la visualización
+            # Añadir un pequeño retraso para mejorar la visualización
+            time.sleep(0.1)
 
             if cv.waitKey(10) & 0xFF == ord('q'):
                 break
@@ -205,18 +304,23 @@ def capture_camera():
         cv.destroyAllWindows()
 
 # Función para realizar predicciones en los frames de video
+
+
 def predict(frame):
     # Obtener las predicciones de los bibs y números en el frame actual
-    output = get_rbns(frame, bd_configPath, bd_weightsPath, bd_classes, nr_configPath, nr_weightsPath, nr_classes)
+    output = get_rbns(frame, bd_configPath, bd_weightsPath,
+                      bd_classes, nr_configPath, nr_weightsPath, nr_classes)
 
     # Anotar el frame con las predicciones si las hay
     if output is not None:
         for detection in output:
             (x, y, w, h) = detection[1]
             cv.rectangle(frame, (x, y), (x + w, y + h), pred_color, 4)
-            cv.putText(frame, str(detection[0]), (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 6, text_color, 6)
+            cv.putText(frame, str(
+                detection[0]), (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 6, text_color, 6)
 
     return frame
 
-if __name__ == "__main__":
-    capture_camera()
+
+# Mostrar la barra de navegación y el contenido de la página
+create_navbar()
